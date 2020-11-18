1a9c8eb03303d9a615289f8e4c1a2412ae7d383c
PaulAmb325 <63868715+PaulAmb325@users.noreply.github.com>
2020-11-18 14:38:16 +0100

    finished game

--- src/Window_Content/Proj_Game.js
+++ src/Window_Content/Proj_Game.js
@@ -18,7 +18,7 @@ function ReadMe_Wind() {
             I really want to continue and finish this game but sadly the amount of time needed to make a game alone it too high.
             </Fieldset>
             <div className="game_info">
-              <div className="game_gif"></div>
+              <div className="game_gif"><img className="concept_gif" src={process.env.PUBLIC_URL+'game_g1.gif'}/></div>
               <Fieldset className="game_concept" label ="Concept">
               You wake up in an obscure dungeon, they are no way out except to continue forward. Pick up your sword and go explore the dungeon while trying to survive the enemy! Use the coins you collect on them to equip yourself better.<br />
               But take care of the shadow of the dungeon, you don’t know what hide behind the doors. <br />
@@ -27,27 +27,36 @@ function ReadMe_Wind() {
             </div>
             <Fieldset className="game_features" label ="Features">
               <Tabs value={tab.activeTab} onChange={handleChange}>
-                <Tab value={0}>Mobs</Tab>
-                <Tab value={1}>Coins</Tab>
-                <Tab value={2}>Light</Tab>
+                <Tab value={0}>Mobs</Tab> 
+                <Tab value={1}>Light</Tab>
+                <Tab value={2}>Coins and Sword</Tab>
               </Tabs>
               <TabBody style={{position:'relative'}}>
                       {tab.activeTab === 0 && (
                         <div className="game_props" style={{ height: 'auto' }}>
-                          <img className="game_props_gif" src={process.env.PUBLIC_URL+'cw_postman.png'}/>
-                          <p className="game_props_txt">We used Postman to keep track of the queries and have example for others to use the API. It also enabled us to run test in both the dev environment and production one according to circumstances. </p>
-                        </div>
-                      )}
-                      {tab.activeTab === 1 && (
-                        <div className="game_coins" style={{ height: 'auto' }}>
-                            <img className="game_coins_gif" src={process.env.PUBLIC_URL+'cw_postman.png'}/>
-                            <p className="game_coins_txt">We used Postman to keep track of the queries and have example for others to use the API. It also enabled us to run test in both the dev environment and production one according to circumstances. </p>
+                          <div className="game_props_gif_cont"> <img className="game_props_gif" src={process.env.PUBLIC_URL+'game_mob.gif'}/> </div>
+                          <p className="game_props_txt">Creating mobs AI was one of the hardest parts particularly the path finding. In fact, the heuristic was uncommon, the mob can not move to win as quick as possible as it could be frustrating for the player, it should also not behave in a too bad way as it is no challenge for the player.  </p>
                         </div>
                       )}
                       {tab.activeTab === 2 && (
+                        <div className="game_coins" style={{ height: 'auto' }}>
+                          <div className="game_coins_gif_cont">
+                            <img className="game_coins_gif" src={process.env.PUBLIC_URL+'game-coins.gif'}/>
+                          </div>
+                            <div className="game_coins_txt">
+                            <p>Coins was also a strange point and the realization was quite mathematical, from coins dropping to coins picking I tried to make it as satisfying as possible.</p>
+                            <p><br />Sword using was also a tricky part as the sword position and angle is calculated based on both the player position, mouse position and attack. Because this formula is run every frame it was needed to be optimized as much as possible.</p>
+                            </div>
+                        </div>
+                      )}
+                      {tab.activeTab === 1 && (
                           <div className="game_light" style={{ height: 'auto' }}>
-                            <img className="game_light_gif" src={process.env.PUBLIC_URL+'cw_postman.png'}/>
-                            <p className="game_light_txt">We used Postman to keep track of the queries and have example for others to use the API. It also enabled us to run test in both the dev environment and production one according to circumstances. </p>
+                          
+                            <p className="game_light_txt">Indisputably the hardest part was to create the light I inspired myself of shaders to create it. It is not perfect by any means but it us player position to calculate the area that should be lighted (no darker shade) and item of a class to be shadow creator. It was optimized to not process every pixels but work with quadrilateral.</p>
+
+                            <div className="game_light_gif_cont">
+                            <img className="game_light_gif" src={process.env.PUBLIC_URL+'game_light.gif'}/>
+                          </div>
                           </div>
                       )}
               </TabBody>
